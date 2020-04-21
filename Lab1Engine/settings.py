8482b34c0cec9a244655695780f17153925ba50f
Danil <antonovdanil2000@gmail.com>
2020-04-22 02:09:14 +0300

    Added mysql support. Fixed some buggs

--- Lab1Engine/settings.py
+++ Lab1Engine/settings.py
@@ -75,13 +75,20 @@ WSGI_APPLICATION = 'Lab1Engine.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_lab1_var1',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
